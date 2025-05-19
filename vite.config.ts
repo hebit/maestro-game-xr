14127ea3df0364534015973a5145b7b385f713fa
hebit <oliveira.hebert@ufba.br>
2025-05-18 21:41:36 -0300

    chore: add path prefix for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/maestro-game-xr/",
   plugins: [react(), basicSsl()],
   resolve: {
     dedupe: ["@react-three/fiber", "three"],
